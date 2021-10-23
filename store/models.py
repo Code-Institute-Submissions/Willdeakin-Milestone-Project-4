c5b7d0a73ac40e56683a9b5621277467664bed53
Willdeakin <willdeakin@outlook.com>
2021-10-23 11:40:19 +0000

    started model for products db

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class products (models.Model):
+
+    brand = models.CharField(max_length=254, null=True, blank=True)
+    product	= models.CharField(max_length=254, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description	= models.TextField()
+    category = models.CharField(max_length=254, null=True, blank=True)
+    serving	= models.CharField(max_length=254, null=True, blank=True)
+    adjustable = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
