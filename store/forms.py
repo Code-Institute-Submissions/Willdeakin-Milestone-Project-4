11b9e3e2d121d1640740b616b60f2d99743ba17d
Willdeakin <willdeakin@outlook.com>
2021-10-25 14:01:45 +0000

    added products to store model and store view

---
+++ store/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Products
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Products
+        fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
--- store/models.py
+++ store/models.py
@@ -7,7 +7,6 @@ class Products(models.Model):
 
     brand = models.CharField(max_length=254, null=True, blank=True)
     product	= models.CharField(max_length=254, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description	= models.TextField()
@@ -16,4 +15,4 @@ class Products(models.Model):
     adjustable = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.product
